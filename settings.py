d8cf564961de8cd37751bab9293287cb563f20cc
ABHINAV  SHARMA <as0673505@gmail.com>
2025-04-13 19:03:21 +0530

    Add 'marketplace' app to INSTALLED_APPS and update urlpatterns

--- settings.py
+++ settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'marketplace',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
--- urls.py
+++ urls.py
@@ -23,7 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('marketplace/', include('marketplace.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
