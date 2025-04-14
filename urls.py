5b4d52cc610d5a7343417776c85556a00342d9f2
ABHINAV  SHARMA <as0673505@gmail.com>
2025-04-14 17:48:37 +0530

    Initial project setup with basic settings and URL configuration

--- urls.py
+++ urls.py
@@ -25,3 +25,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('marketplace/', include('marketplace.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
